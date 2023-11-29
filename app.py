cb771378bce4592cf96b2f7a0094fd4d1699262f
ramsjenu <vramschennai@gmail.com>
2023-11-29 15:14:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,18 @@
-from flask import Flask
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
 app = Flask(__name__)
 
-# add a rule for the index page.
-app.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+## Route for a home page
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-app.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+##@app.route('/')
 
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    app.debug = True
-    app.run()
+def index():
+    return 'Hello'     
+
+if __name__=="__main__":
+    app.run()        
