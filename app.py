266cbe21f620a855722f0e27c7287fcf889fe38b
ramsjenu <vramschennai@gmail.com>
2023-11-29 14:58:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,37 +12,29 @@ app=Flask(__name__)
 
 ## Route for a home page
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
+##@app.route('/')
+##def index():
+##    return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+# some bits of text for the page.
 
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
-        predict_pipeline=PredictPipeline()
-        print(pred_df)
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
     
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+
+# add a rule for the index page.
+app.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
+
 
 if __name__=="__main__":
        app.run()
