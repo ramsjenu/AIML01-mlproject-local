cf5981513ea8c1635250c44725c690d13a59fb87
ramsjenu <vramschennai@gmail.com>
2023-11-29 10:39:23 +0000

    Update application.py

--- application.py
+++ application.py
@@ -9,43 +9,7 @@ application=Flask(__name__)
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-#app=application
-
-## Route for a home page
-
-##@app.route('/')
-##def index():
-##    return render_template('index.html') 
-
-#@app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
-        predict_pipeline=PredictPipeline()
-        print(pred_df)
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
+    say_hello() + instructions + footer_text))    
 
 if __name__=="__main__":
     application.debug = True
