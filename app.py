306e13e61739eebd9806dd15536bc14fd4a14b82
ramsjenu <vramschennai@gmail.com>
2023-11-29 15:24:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,27 +18,7 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
-        predict_pipeline=PredictPipeline()
-        print(pred_df)
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
+        return render_template('index.html')
     
 
 if __name__=="__main__":
